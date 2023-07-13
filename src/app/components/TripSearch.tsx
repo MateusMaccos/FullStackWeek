bfcf82b71dabbe200320ba3e2ade6f54249847ee
Mateus Machado Costa <mateusdoug@hotmail.com>
2023-07-13 11:25:19 -0300

    fix: center tripsearch

--- src/app/components/TripSearch.tsx
+++ src/app/components/TripSearch.tsx
@@ -7,7 +7,7 @@ import React from 'react'
 const TripSearch = () => {
     return (
         <div className='container mx-auto p-5 bg-search-background bg-cover bg-center bg-no-repeat'>
-            <h1 className='font-semibold text-2xl text-primaryDarker'>Encontre sua próxima <span className='text-primary'>viagem!</span></h1>
+            <h1 className='font-semibold text-2xl text-primaryDarker text-center lg:text-[2.5rem]'>Encontre sua próxima <span className='text-primary'>viagem!</span></h1>
             <div className='flex flex-col gap-4 mt-5'>
                 <Input placeholder='Onde você quer ir?' />
                 <div className='flex gap-4'>
