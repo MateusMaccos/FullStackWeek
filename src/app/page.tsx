c63c3e8f315db1670791dcd95abe63d3d559547d
Mateus Machado Costa <mateusdoug@hotmail.com>
2023-07-13 09:53:01 -0300

    add: init search trip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 "use client"
 
-import { signIn, signOut, useSession } from 'next-auth/react'
+import TripSearch from "./components/TripSearch"
 
 export default function Home() {
-  const { data } = useSession()
 
   return (
-    <div className='bg-primary'>
+    <div>
+      <TripSearch />
     </div>
   )
 }
