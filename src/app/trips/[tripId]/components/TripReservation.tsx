f2a68d5f2ffe775a1b7c36d50de204740813e37d
Mateus Machado Costa <mateusdoug@hotmail.com>
2023-07-14 15:04:57 -0300

    style: add border to trip reservation

--- src/app/trips/[tripId]/components/TripReservation.tsx
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -11,24 +11,27 @@ interface TripReservationProps {
 
 const TripReservation = ({ trip }: TripReservationProps) => {
     return (
-        <div>
-            <div className="flex flex-col px-5">
-                <div className="flex gap-4">
-                    <DatePicker placeholderText='Data de Início' onChange={() => { }} className='w-full' />
-                    <DatePicker placeholderText='Data Final' onChange={() => { }} className='w-full' />
-                </div>
-                <Input placeholder={`Número de hóspedes (max ${trip.maxGuests})`} className='mt-4' />
 
-                <div className="flex justify-between mt-3">
-                    <p className='font-medium text-sm text-primaryDarker'>Total: </p>
-                    <p className='font-medium text-sm text-primaryDarker'>R$2500</p>
-                </div>
-                <Button className='mt-3'>
+        <div className="flex flex-col px-5">
+            <div className="flex gap-4">
+                <DatePicker placeholderText='Data de Início' onChange={() => { }} className='w-full' />
+                <DatePicker placeholderText='Data Final' onChange={() => { }} className='w-full' />
+            </div>
+            <Input placeholder={`Número de hóspedes (max ${trip.maxGuests})`} className='mt-4' />
+
+            <div className="flex justify-between mt-3">
+                <p className='font-medium text-sm text-primaryDarker'>Total: </p>
+                <p className='font-medium text-sm text-primaryDarker'>R$2500</p>
+            </div>
+            <div className='pb-10 border-b border-grayLighter w-full'>
+                <Button className='w-full mt-3'>
                     Reservar agora
                 </Button>
-
             </div>
+
+
         </div>
+
     )
 }
 
