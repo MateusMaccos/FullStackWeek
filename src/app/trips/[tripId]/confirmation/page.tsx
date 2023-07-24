725003faee66d5d82a3d5d819ed061874f0af59a
Mateus Machado Costa <mateusdoug@hotmail.com>
2023-07-24 15:59:14 -0300

    style:adapt trip confirmation to desktop

--- src/app/trips/[tripId]/confirmation/page.tsx
+++ src/app/trips/[tripId]/confirmation/page.tsx
@@ -90,7 +90,7 @@ const TripConfirmation = ({ params }: { params: { tripId: string } }) => {
     const guests = searchParams.get("guests")
 
     return (
-        <div className="container mx-auto p-5">
+        <div className="container mx-auto p-5 lg:max-w-[600px]">
             <h1 className='font-semibold text-xl text-primaryDarker'>Sua viagem</h1>
             <div className="flex flex-col p-5 mt-5 border-grayLighter border-solid border shadow-lg rounded-lg">
                 <div className="flex items-center gap-3 pb-5 border-b border-grayLighter border-solid">
