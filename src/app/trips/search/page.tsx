98fbb3d294f6ca88e24adda5a07227fc119fdb5a
Mateus Machado Costa <mateusdoug@hotmail.com>
2023-07-24 15:28:32 -0300

    style: adapt search page to desktop

--- src/app/trips/search/page.tsx
+++ src/app/trips/search/page.tsx
@@ -23,10 +23,10 @@ const Trips = () => {
     }, [])
 
     return (
-        <div className='container mx-auto flex flex-col p-5 items-center'>
-            <h1 className='text-primaryDarker font-semibold text-xl'>Viagens Encontradas</h1>
-            <h2 className='text-grayPrimary font-medium mb-5'>{trips.length > 0 ? `Listamos as melhores viagens com a busca: ${searchParams.get("text")}!` : 'Não encontramos nada nos seus parâmetros'}</h2>
-            <div className="flex flex-col gap-4">
+        <div className='container mx-auto flex flex-col p-5 items-center lg:items-start'>
+            <h1 className='text-primaryDarker font-semibold text-xl lg:text-left lg:text-[2.5rem]'>Viagens Encontradas</h1>
+            <h2 className='text-grayPrimary font-medium mb-5 lg:mt-6 lg:w-full lg:text-left'>{trips.length > 0 ? `Listamos as melhores viagens com a busca: ${searchParams.get("text")}!` : 'Não encontramos nada nos seus parâmetros'}</h2>
+            <div className="flex flex-col gap-4 lg:grid lg:grid-cols-4 lg:gap-10 lg:mt-6 lg:pb-16">
                 {trips?.map(trip => <TripItem key={trip.id} trip={trip} />)}
             </div>
         </div>
